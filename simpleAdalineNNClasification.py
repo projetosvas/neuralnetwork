018f7eb6aa579b12baba9047548f4f949e7a8223
VAS <projetos.vas@gmail.com>
2025-03-28 09:01:00 -0300

    Add Perceptron for classification problems and Analine for classification problems

--- simpleAdalineNNClasification.py
+++ simpleAdalineNNClasification.py
@@ -1,12 +1,12 @@
-## Simple Layer Perceptron Network - Classification Problem
+## Simple Layer Adaline Network - Classification Problem
 # Created by: Vinicius Amorim Santos 
-# Date: 27/03/2025
+# Date: 28/03/2025
 # Version: 1.0.0
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-class Perceptron():
+class Adaline():
     def __init__(self, inputSize, learningRate, epochs):
         self.inputSize = inputSize # Size of Input Vector (Number of Features)
         self.lr = learningRate # Learning Rate
@@ -25,10 +25,10 @@ class Perceptron():
         for i in range(self.epochs):
             print(f'Weight: {self.weights} Bias: {self.bias} epoch: {i}')
             for xi, target in zip(X, y):
-                output = self.step(np.dot(xi, self.weights) + self.bias)
-                self.error = (target - output)
-                self.weights = self.weights + self.lr * (self.error) * xi
-                self.bias = self.bias + self.lr * self.error
+                output = np.dot(xi, self.weights) + self.bias
+                self.error = target - output
+                self.weights += self.lr * (self.error) * xi
+                self.bias += self.lr * self.error
 
 
     def predict(self,X):
@@ -56,13 +56,13 @@ y = np.array([1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0])
 
 # Train
 
-print('Treino Perceptron:')
-perceptron = Perceptron(2, 0.1, 10)
-perceptron.fit(X, y) 
+print('Treino Adaline:')
+adaline = Adaline(2, 0.1, 10)
+adaline.fit(X, y) 
 
 
 x1 = np.linspace(-2, 6, 100)  
-x2 = -(perceptron.weights[0] / perceptron.weights[1]) * x1 - (perceptron.bias / perceptron.weights[1])
+x2 = -(adaline.weights[0] / adaline.weights[1]) * x1 - (adaline.bias / adaline.weights[1])
 
 colors = np.array(['red', 'blue'])
 
